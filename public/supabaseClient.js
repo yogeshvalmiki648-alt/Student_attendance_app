de7d6583d0c299b3d0d6b5b2a36d171f96c69b22
Yogesh <yogeshvalmiki648@gmail.com>
2026-06-30 09:44:39 +0530

    Create supabaseClient.js

---
+++ public/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://zhmgdpzfwsopqucgflnt.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_PztXL0-fk15GYLYPx0sMxw_-HDGiXTF";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
